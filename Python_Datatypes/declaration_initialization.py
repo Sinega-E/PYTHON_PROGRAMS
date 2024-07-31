39fa0533c646f4fab1e69605289c8448bb8112fc
Sinega-E <sinega.elanthiraiyan@gmail.com>
2024-07-31 11:54:16 +0530

    commit

---
+++ Python_Datatypes/declaration_initialization.py
@@ -0,0 +1,21 @@
+#basic declaration and initialization
+
+a = 10
+print(a)
+
+#declaring multiple variables
+
+b,c,d = 4,"hello",7.89
+print(a)
+print(b)
+print(c)
+print(d)
+type(a)
+type(b)
+type(c)
+type(d)
+
+print(a.__sizeof__())
+print(b.__sizeof__())
+print(c.__sizeof__())
+print(d.__sizeof__())
