6568709a36500e0d02827d9644fecd3ee50acb9c
Sinega-E <sinega.elanthiraiyan@gmail.com>
2025-03-06 12:51:24 +0530

    commit

---
+++ Python_Datatypes/loop.py
@@ -0,0 +1,19 @@
+# str=["Hello World",1,3,45,6,7,8]
+# # b={
+# #     "name":"priya",
+# #     "age":35
+# # }
+# # print(len(str))
+# for i in str: 
+#     if i == 45:
+#         continue
+#         print("found")
+        
+#     else:
+#         print("not found")
+# # a=[5,8,9,9,9,80]
+
+# # print(a)
+
+a="Hello World"
+print(a[0:5])
